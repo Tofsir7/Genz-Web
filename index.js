8c590ee85bfcf1288077407777264604d6cb9386
Khayrul Hassan Khan <khtofsir9191@gmail.com>
2025-01-14 00:44:15 +0600

    added placeholder

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const server = http.createServer((req,res)=>{
   const {query,pathname} = url.parse(req.url,true);
   console.log(pathname);
   if(pathname==='/' || pathname==='/home'){
-    //res.writeHead(200, {"content-type":"text/html"});
+    res.writeHead(200, {"content-type":"text/html"});
     const output = tempHome;
+    const path = "Images\\GenZ logo.png"
+    output = output.replace(/{%logo%}/g,path);
+    res.end(output);
+    console.log(output);
+  }
+  else if(pathname==='/Banner.jpg'){
+    const banner =  `<img src="${__dirname}\\Images\\Banner.jpg" id="place"> </img>`;
+    output= tempHome;
+    output = output.replace(/{%banner%}/g, banner);
     res.end(output);
     //console.log(output);
   }
-  else if(pathname==='/Banner.jpg'){
-    const output =  `<img src="Images\\Banner.jpg" id="place"> </img>`;
-    res.end(output);
+  else if (pathname === '/favicon.ico') {
+    res.writeHead(204);
+    res.end();
   }
   else{
     res.end("Matha Nosto");
@@ -23,4 +32,5 @@ const server = http.createServer((req,res)=>{
 
 server.listen(9000, '127.0.0.1', ()=>{
   console.log("Runing on port number : 9000");
+  console.log(__dirname);
 });

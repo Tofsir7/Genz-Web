e8b7c8160d0e3e935b5ec64652400f7d965d0353
Khayrul Hassan Khan <khtofsir9191@gmail.com>
2025-03-15 23:17:05 +0600

    express added

--- index.js
+++ index.js
@@ -2,80 +2,87 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 const path = require("path");
-const replaceTemplate = require("./Modules/replaceTemplate");
-const tempHome = fs.readFileSync(path.join(__dirname, "Page.html"), "utf8");
-const tempCourse = fs.readFileSync(
-  path.join(__dirname, "Templates/Template-course.html"),
-  "utf8"
-);
-const data = fs.readFileSync("Dev-data/data.json");
-const enrollForm = fs.readFileSync(
-  path.join(__dirname, "Templates/Enroll-form.html"),
-  "utf8"
-);
-const dataObject = JSON.parse(data);
-//const tempHome = fs.readFileSync("Page.html", 'utf8');
-const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true);
-  //console.log(query);
-
-  //home page
-  if (pathname === "/" || pathname === "/home") {
-    res.writeHead(200, { "content-type": "text/html" });
-    let output = tempHome;
-    let path = `"https://raw.githubusercontent.com/Tofsir7/Genz-Web/refs/heads/master/Images/GenZ-logo.png"`;
-    output = output.replace(/{%logo%}/g, path);
-    output = output.replace(/{%banner%}/g, "");
-    res.write(output);
-    //console.log(req.url);
-  }
-
-  // course overview page
-  else if (pathname === "/Banner.jpg") {
-    res.writeHead(200, { "content-type": "text/html" });
-    const banner = `<img src='Images/Banner.jpg'> </img>`;
-    let output = tempHome;
-    output = output.replace(/{%banner%}/g, banner);
-    res.write(output);
-  } else if (pathname === "/favicon.ico") {
-    res.writeHead(204, { "content-type": "text/html" });
-    res.write("This is default");
-  }
-
-  //course details page
-  else if (pathname === "/course") {
-    res.writeHead(200, { "content-type": "text/html" });
-    const coursePage = dataObject
-      .map((el) => replaceTemplate(tempCourse, el))
-      .join("");
-    const course = dataObject[query.id];
-    const output = replaceTemplate(tempCourse, course);
-    //console.log();
-    res.write(output);
-  }
-
-  //Enroll form
-
-  if (pathname === "/enroll") {
-    res.writeHead(200, { "content-type": "text/html" });
-    const output = enrollForm;
-    res.write(output);
-  }
-
-  //API page
-  else if (pathname === "/api") {
-    res.writeHead(200, { "content-type": "application/json" });
-
-    res.write(data);
-    //res.write('API call');
-  } else {
-    //res.write("Matha Nosto");
-  }
-  res.end();
-  //console.log(query.course);
+const express = require("express")
+const app = express();
+const replaceTemplate = require("./Modules/replaceTemplate");  // rtemplate replace korar module ta require kora hoyeche
+const tempHome = fs.readFileSync(path.join(__dirname, "Page.html"), "utf8"); // home er template ta file the read kora hoyeche
+const tempCourse = fs.readFileSync(path.join(__dirname, "Templates/Template-course.html"), "utf8"); // course er template
+const welcomeCard = fs.readFileSync(path.join(__dirname, "Templates/welcome-card.html"), "utf8");
+const data = fs.readFileSync("Dev-data/data.json"); // data gula read kora hoyeche as STRING format
+const enrollForm = fs.readFileSync(path.join(__dirname, "Templates/Enroll-form.html"), "utf8"); // enroll korar form
+const dataObject = JSON.parse(data); // data converted into JSON format
+app.use(express.json())
+//homepage
+app.get(['/home', '/'], (req, res) => {
+  res.set('Content-Type', 'text/html');
+  let output = tempHome;
+  let path = `"https://raw.githubusercontent.com/Tofsir7/Genz-Web/refs/heads/master/Images/GenZ-logo.png"`;
+  output = output.replace(/{%logo%}/g, path);
+  res.status(200).send(output);
 });
-const PORT = process.env.PORT || 9000;
-server.listen(PORT, "0.0.0.0", () => {
-  console.log("Runing on port number : 9000");
-  //console.log(__dirname);
+// exploring the courses
+app.get('/course', (req, res) => {
+  const { query } = url.parse(req.url, true);
+  const myobj = query;
+  res.set('Content-Type', 'text/html');
+  const coursePage = dataObject
+    .map((el) => replaceTemplate(tempCourse, el))
+    .join("");
+  const course = dataObject[query.id];
+  const output = replaceTemplate(tempCourse, course);
+  res.write(output);
 });
+
+//enroll course
+app.get('/enroll', (req, res) => {
+  res.set('content-type', "text/html");
+  const output = enrollForm;
+  res.write(output);
+});
+
+app.get('/api', (req, res) => {
+  res.set('content-type', 'application/json')
+  res.send(data);
+});
+
+app.get('/register', async (req, res) => {
+  console.log(req.query);
+  const student = new Student(req.body);
+  console.log(student);
+});
+
+//form data submission
+// app.post('/register', async (req, res) => {
+//   try {
+//     console.log(req.body)
+//     const student = new Student(req.body);
+//     await student.save();
+//     res.status(201).json(student);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
+
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  dob: { type: Date, required: true },
+  gender: { type: String, enum: ['male', 'female', 'other'], required: true },
+  course: { type: String, required: true }
+});
+
+const Student = mongoose.model('User', studentSchema);
+
+module.exports = Student;
+
+
+
+
+// start server
+const port = 9000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`App is running on port: ${port}`);
+})

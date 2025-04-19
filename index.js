60856cd36074de90c0c8e7be3a8932ff99b1f5e1
Khayrul Hassan Khan <khtofsir9191@gmail.com>
2025-04-19 11:53:17 +0600

    package added

--- index.js
+++ index.js
@@ -317,9 +317,15 @@ app.post('/submit-contact', async (req, res) => {
     // Creating a new contact and save to the database
     const contact = new Contact({ fullName, email, message });
     await contact.save();
-    const messages = await Contact.findOne({ email }).select({ fullName: 1, message: 1 });
-    console.log(messages);
-    const output = replaceTemplate(contactMessagesPage, messages);
+    const messages = await Contact.find({ email }).select({ fullName: 1, message: 1 });
+    //console.log(messages);
+    //const name = messages[0].fullName;
+    let messageContent = "";
+    for (let i = 0; i < messages.length; i++) {
+      messageContent += `<p>${messages[i].message}</p>`;
+    }
+    const output = contactMessagesPage.replace(/{%MAIL%}/g, email).replace(/{%MESSAGES%}/g, messageContent);
+    //const output = replaceTemplate(contactMessagesPage, messages);
     //console.log(messages);
     // Send a success response
     res.status(200).send(output);
